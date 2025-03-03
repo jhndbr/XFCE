24efd4c3959c033d9315bf05713b3d76fa926775
Jhon Aroa <125928609+jhndbr@users.noreply.github.com>
2025-03-03 01:43:43 -0300

    Add files via upload

---
+++ personalizacion.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/vinceliuice/Layan-gtk-theme.git
+git clone https://github.com/vinceliuice/Orchis-theme.git
+git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
+git clone https://github.com/vinceliuice/Fluent-icon-theme.git
