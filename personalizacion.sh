7979348891c3e1fbf3cf7006ca3eeb05c887ac51
Jhon Aroa <125928609+jhndbr@users.noreply.github.com>
2025-08-14 20:33:47 -0300

    Mejora: automatiza instalación y limpieza en el script de personalización

--- personalizacion.sh
+++ personalizacion.sh
@@ -1,4 +1,38 @@
-git clone https://github.com/vinceliuice/Layan-gtk-theme.git
-git clone https://github.com/vinceliuice/Orchis-theme.git
-git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
-git clone https://github.com/vinceliuice/Fluent-icon-theme.git
+#!/bin/bash
+
+set -e
+
+THEMES_DIR="$HOME/.themes"
+ICONS_DIR="$HOME/.icons"
+WORKDIR="$(pwd)"
+
+echo "\n[+] Creando carpetas de temas e iconos si no existen..."
+mkdir -p "$THEMES_DIR" "$ICONS_DIR"
+
+echo "\n[+] Clonando y aplicando temas GTK..."
+git clone --depth=1 https://github.com/vinceliuice/Layan-gtk-theme.git
+cd Layan-gtk-theme
+./install.sh -d "$THEMES_DIR"
+cd "$WORKDIR"
+rm -rf Layan-gtk-theme
+
+git clone --depth=1 https://github.com/vinceliuice/Orchis-theme.git
+cd Orchis-theme
+./install.sh -d "$THEMES_DIR"
+cd "$WORKDIR"
+rm -rf Orchis-theme
+
+echo "\n[+] Clonando y aplicando temas de iconos..."
+git clone --depth=1 https://github.com/vinceliuice/WhiteSur-icon-theme.git
+cd WhiteSur-icon-theme
+./install.sh -d "$ICONS_DIR"
+cd "$WORKDIR"
+rm -rf WhiteSur-icon-theme
+
+git clone --depth=1 https://github.com/vinceliuice/Fluent-icon-theme.git
+cd Fluent-icon-theme
+./install.sh -d "$ICONS_DIR"
+cd "$WORKDIR"
+rm -rf Fluent-icon-theme
+
+echo "\n[✔] Personalización completada. Puedes seleccionar los temas e iconos desde la configuración de XFCE."
